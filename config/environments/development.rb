c280a56ee53725ef997f6779aa14cdfb7c0f5562
mikeomeara <mikeytoomo@hotmail.com>
2015-05-22 15:26:10 +0100

    Adding compile code to make comple = true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+    # To fix problem where app was not showing up properly
+    config.assets.compile = true
   # Do not eager load code on boot.
   config.eager_load = false
 
